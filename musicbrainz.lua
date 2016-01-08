54f9a8c5437059027a16be9c9166fa59b5935716
Arkiver2 <Arkiver@hotmail.com>
2016-01-09 00:56:04 +0100

    initial

---
+++ musicbrainz.lua
@@ -0,0 +1,68 @@
+local url_count = 0
+local tries = 0
+
+local downloaded = {}
+local addedtolist = {}
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  local url = urlpos["url"]["url"]
+  local html = urlpos["link_expect_html"]
+  
+  if downloaded[url] ~= true and addedtolist[url] ~= true then
+    addedtolist[url] = true
+    return true
+  else
+    return false
+  end
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  -- NEW for 2014: Slightly more verbose messages because people keep
+  -- complaining that it's not moving or not working
+  status_code = http_stat["statcode"]
+  
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. ".  \n")
+  io.stdout:flush()
+
+  if downloaded[url["url"]] == true then
+    return wget.actions.EXIT
+  end
+
+  if (status_code >= 200 and status_code <= 399) then
+    if string.match(url.url, "https://") then
+      local newurl = string.gsub(url["url"], "https://", "http://")
+      downloaded[newurl] = true
+    else
+      downloaded[url["url"]] = true
+    end
+  end
+  
+  if status_code >= 500 or
+    (status_code >= 400 and status_code ~= 404 and status_code ~= 403 and status_code ~= 400) or 
+    status_code == 0 then
+    io.stdout:write("Server returned "..http_stat.statcode.." ("..err.."). Sleeping.\n")
+    io.stdout:flush()
+    os.execute("sleep 1")
+    tries = tries + 1
+    if tries >= 5 then
+      io.stdout:write("\nI give up...\n")
+      io.stdout:flush()
+      tries = 0
+      return wget.actions.EXIT
+    else
+      return wget.actions.CONTINUE
+    end
+  end
+
+  tries = 0
+
+  local sleep_time = 0
+
+  if sleep_time > 0.001 then
+    os.execute("sleep " .. sleep_time)
+  end
+
+  return wget.actions.NOTHING
+end
+
